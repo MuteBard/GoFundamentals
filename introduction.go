6e1ba8dd32872838244639e9b133299380db7454
MuteBard <csevere@brandeis.edu>
2019-09-24 19:49:52 -0400

    first go program

---
+++ introduction.go
@@ -0,0 +1,15 @@
+//introduction.go
+package main
+
+//Go is a very simple language, and by itself, does not have many built-in functions. Most of the code you'll be using will be in packages.
+//The fmt package contains functions for formatted printing.
+import (
+	"fmt"
+)
+//We define a function with the func keyword.
+//unlike java, you do not need to place a semi-colon at the end of the line
+func main(){
+	fmt.Println("Hello world")
+}
+
+//execute by entering go run introduction.go

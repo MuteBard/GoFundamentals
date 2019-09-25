fda4db5068434edef15eef5f31d772c5b188daa1
MuteBard <csevere@brandeis.edu>
2019-09-25 11:08:04 -0400

    might be my prefered way to right conditions tbh

--- conditions.go
+++ conditions.go
@@ -2,13 +2,27 @@ package main
 import("fmt")
 func main(){
 	x := 10
+	y := 20
 
 	if x > 5 {
 		fmt.Println("idk how I feel about the absence of parentheses")
 	}else{
 		fmt.Println("but ill get over it")
 	}
+
+	condition := x + y > 25 && y > 15 
+	
+	if condition {
+		fmt.Println("its doing the thing")
+		fmt.Printf("condition is type %T\n", condition)
+	}
+
+
+
+
 }
 
 // $ go run conditions.go
 // idk how I feel about the absence of parentheses
+
+

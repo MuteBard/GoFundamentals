698fc1d4d7398a663e97009955ae1ae9a1090433
MuteBard <csevere@brandeis.edu>
2019-09-25 11:15:01 -0400

    if-statements can have an initialization step

--- conditions.go
+++ conditions.go
@@ -17,12 +17,24 @@ func main(){
 		fmt.Printf("condition is type %T\n", condition)
 	}
 
-
-
+	//in the other direction, if can have an optional initialization statement
+	if anothercondition := x + y; anothercondition == 30{
+		fmt.Println("should be equal to 30")
+	}
 
 }
 
 // $ go run conditions.go
 // idk how I feel about the absence of parentheses
 
+// $ go run conditions.go
+// idk how I feel about the absence of parentheses
+// its doing the thing
+// condition is type bool
+
+// $ go run conditions.go
+// idk how I feel about the absence of parentheses
+// its doing the thing
+// condition is type bool
+// should be equal to 30
 

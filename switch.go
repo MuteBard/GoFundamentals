e5a9d216778d9cecc972c97629f07987bbcd08c6
MuteBard <csevere@brandeis.edu>
2019-09-25 11:22:58 -0400

    added switch.go

---
+++ switch.go
@@ -0,0 +1,38 @@
+package main
+import("fmt")
+func main(){
+
+	x := 2;
+	switch x {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	case 4:
+		fmt.Println("four")
+	default:
+		fmt.Println("idc anymore")
+	}
+
+
+	y := "two";
+	switch y {
+	case "one":
+		fmt.Println(1)
+	case "two":
+		fmt.Println(2)
+	case "three":
+		fmt.Println(3)
+	case "four":
+		fmt.Println(4)
+	default:
+		fmt.Println("69646320616e796d6f7265")
+	}
+
+}
+
+// $ go run switch.go
+// two
+// 2

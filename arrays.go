be2f4957646fdf3862682bf865b5a0ee39c2e988
MuteBard <csevere@brandeis.edu>
2019-09-25 19:34:33 -0400

    added arrays.go

---
+++ arrays.go
@@ -0,0 +1,8 @@
+package main
+import("fmt")
+func main() {
+	tanks := []string{"Paladin", "Dark Knight", "GunBreaker", "Warrior"}
+	fmt.Println(tanks)
+	fmt.Println(len(tanks))
+	fmt.Println(tanks[2])
+}

6bff2ca0f0c4664c8fcdf175cf91bce63722f2f7
MuteBard <csevere@brandeis.edu>
2019-09-25 10:55:10 -0400

    added conditions.go

---
+++ conditions.go
@@ -0,0 +1,14 @@
+package main
+import("fmt")
+func main(){
+	x := 10
+
+	if x > 5 {
+		fmt.Println("idk how I feel about the absence of parentheses")
+	}else{
+		fmt.Println("but ill get over it")
+	}
+}
+
+// $ go run conditions.go
+// idk how I feel about the absence of parentheses

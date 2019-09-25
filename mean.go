38382245b9e713bd36c21f345d74e9ab9c645aa0
MuteBard <csevere@brandeis.edu>
2019-09-24 20:32:24 -0400

    using floats to calculate mean

--- mean.go
+++ mean.go
@@ -2,16 +2,16 @@ package main
 import("fmt")
 
 func main(){
-	var x int
-	var y int
+	var x float64
+	var y float64
 
 	x = 4
-	y = 12
+	y = 19
 
 	fmt.Printf("x = %v, type of %T\n", x, x)
 	fmt.Printf("y = %v, type of %T\n", y, y)
 
-	var mean int
+	var mean float64
 	mean = (x + y) / 2
 	fmt.Printf("mean = %v, type of %T\n", mean, mean)
 }
@@ -20,3 +20,10 @@ func main(){
 // x = 4, type of int
 // y = 12, type of int
 // mean = 8, type of int
+
+// $ go run mean.go
+// x = 4, type of float64
+// y = 19, type of float64
+// mean = 11.5, type of float64
+
+

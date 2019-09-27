4107f2156dd81e13c78c5f269e088d0572e1090a
MuteBard <csevere@brandeis.edu>
2019-09-27 13:43:33 -0400

    added newDeck function

--- cards/deck.go
+++ cards/deck.go
@@ -12,6 +12,21 @@ type deck []string
 // - d  Instance of the deck variable we're working with is available in the function as a variable called 'd'
 // - deck  Every variable of type deck can call this function on itself
 // by convention we indicate the reciever by a 1 or 2 letter abbrievation that matches the type of the reciever
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	//When theres a value you dont care about, use _ to tell Go that you know that it exists but you dont care about it
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			card := value + " of " + suit
+			cards = append(cards, card)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)

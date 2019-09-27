32fa764e119363491158ab86825afc1b5b23dde0
MuteBard <csevere@brandeis.edu>
2019-09-27 11:47:25 -0400

    understanding OOP vs types in Go

---
+++ cards/deck.go
@@ -0,0 +1,6 @@
+package main
+
+//Create a new type of 'deck'
+//which is a slice of strings
+//this new deck type that we are creating borrows all the behavior of a slice of type string
+type deck []string

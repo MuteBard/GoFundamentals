3ee17e5c4f687cb3356a2de031e251951e197cf9
MuteBard <csevere@brandeis.edu>
2019-09-27 16:11:35 -0400

    Reading from File

--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,9 @@
 package main
 
 func main() {
-	cards := newDeck()
-	cards.saveToFile("my_Cards")
+	// cards := newDeck()
+	// cards.saveToFile("my_Cards")
+
+	cards := newDeckFromFile("my_Cards")
+	cards.print()
 }

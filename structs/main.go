9e588494d808cd46ac8a18f99cb7f8d020faf49d
MuteBard <csevere@brandeis.edu>
2019-09-28 21:21:58 -0400

    recievers, structs, but no pointers

--- structs/main.go
+++ structs/main.go
@@ -10,44 +10,29 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
 
-	//first way to initialize a struct
-	// carl := {"Carl", "Severe"}
-	// fmt.Println(carl)
-
-	//second way to initialize a struct
-	carl := person{firstName: "Carl", lastName: "Severe"}
-	fmt.Println(carl)
-
-	//third way to initialize a struct
-	var erin person
-	fmt.Println(erin)
-
-	//updating a value
-	carl.firstName = "carrrrl"
-
-	//printing with the key : value
-	fmt.Printf("%+v\n", carl)
-	fmt.Printf("%+v\n", erin)
-
 	metagross := person{
 		firstName: "Meta",
 		lastName:  "Gross",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "metagross@gmail.com",
 			zipCode: 94000,
 		},
 	}
 
-	fmt.Printf("%+v\n", metagross)
+	metagross.updateName("MegaMeta")
+	metagross.println()
 }
 
-// $ go run main.go
-// {Carl Severe}
-// { }
-// {firstName:carrrrl lastName:Severe}
-// {firstName: lastName:}
+//As the code is set up now, this will not work as intended, we need to talk about pointers
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) println() {
+	fmt.Printf("%+v\n", p)
+}
